7374e0f61ba7a6537b3fb3165f05098025f542d4
nfoj <nfoj_@hotmail.com>
2024-08-12 23:49:19 -0300

    rust string

--- documentation/rust/syntax-semantics/src/b.rs
+++ documentation/rust/syntax-semantics/src/b.rs
@@ -1,31 +1,22 @@
+use std::io;
+
 pub fn cfconstructs() {
-    // functions
-    let sum: u8 = sum(1, 2);
-    println!("1 + 2 = {}", sum);
-    println!("----------------------------------");
+    let mut name = String::new();
 
-    // dead = let one_ > No use!!
-    //let sub: u8 = sub(4, 3);
-    //println!("4 - 3 = {}", sub);
-    //println!("----------------------------------");
+    println!("Enter your name: ");
+    io::stdin()
+        .read_line(&mut name)
+        .expect("Failed in read line!");
 
-    let myresult: String = get_full_name("Roberto", "Porco");
-    println!("Hello {0}", myresult);
-    println!("----------------------------------");
-}
+    println!("Hello, {}!", name.trim());
 
-fn sum(a: u8, b: u8) -> u8 {
-    a + b
-}
+    println!("Enter you age: ");
+    let mut age = String::new();
+    io::stdin()
+        .read_line(&mut age)
+        .expect("Failed in read line!");
 
-// No use function
-#[allow(dead_code)]
-fn sub(c: u8, d: u8) -> u8 {
-    c - d
-}
+    let age: i8 = age.trim().parse().expect("Failed in read line!");
 
-// &str + &str = String
-fn get_full_name(first: &str, last: &str) -> String {
-    let full_name: String = format!("{0} {1}", first, last);
-    return full_name;
+    println!("You are {} years.", age);
 }
