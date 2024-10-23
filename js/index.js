e3760d9911ab0b2e3d4a6c5b00b9bb174342d344
nfoj <nfoj_@hotmail.com>
2024-10-23 16:53:14 -0300

    init

---
+++ js/index.js
@@ -0,0 +1,15 @@
+function openLeftMenu() {
+    document.getElementById("leftMenu").classList.add("open");
+}
+
+function closeLeftMenu() {
+    document.getElementById("leftMenu").classList.remove("open");
+}
+
+function openRightMenu() {
+    document.getElementById("rightMenu").classList.add("open");
+}
+
+function closeRightMenu() {
+    document.getElementById("rightMenu").classList.remove("open");
+}
